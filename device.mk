a77d5fff738d03aa6d05c66a8446c66b5d1df352
Jerry Zhang <zhangjerry@google.com>
2023-07-18 19:51:49 +0100

    lake: Set sys.usb.ffs.aio_compat in recovery
    The kernel does not support aio with ffs.
    Set the property so adb/mtp know to use the compatible
    functions, and the same while in recovery.
    
    Bug: 37916658
    Test: Use adb / use adb in recovery
    Change-Id: Id35a05fcb9952dcf7239bd15a61cc620537419bc
    Signed-off-by: XeroMz 69 <adamyd18@gmail.com>

--- device.mk
+++ device.mk
@@ -85,6 +85,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6768:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.mt6768
 
+PRODUCT_PACKAGES += \
+    init.recovery.mt6768.rc
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 30
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
