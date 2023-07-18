957ece315daf267e8741f999bad21b5385aee1b0
bengris32 <bengris32@protonmail.ch>
2023-07-18 19:47:40 +0100

    lake: Include `hardware/mediatek` Soong namespace
    Change-Id: I9028cec415568445e4bf6b2cb7cdf3f03e95154d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: XeroMz69 <adamyd18@gmail.com>

--- device.mk
+++ device.mk
@@ -78,7 +78,8 @@ PRODUCT_SHIPPING_API_LEVEL := 34
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/lake/lake-vendor.mk)
