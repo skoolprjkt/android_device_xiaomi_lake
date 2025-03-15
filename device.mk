1d1271b76586339802888e2dd92db5c472a2490e
XeroMz69 <adamyd18@gmail.com>
2025-03-15 11:23:17 +0700

    lake: Initial minimised rootdir
    Signed-off-by: XeroMz69 <adamyd18@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.insmod.sh \
+
+PRODUCT_PACKAGES += \
+    fstab.mt6768 \
+    init.cgroup.rc \
+    init.connectivity.common.rc \
+    init.connectivity.rc \
+    init.insmod.mtk.cfg \
+    init.modem.rc \
+    init.mt6768.rc \
+    init.mt6768.usb.rc \
+    init.mtkgki.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    init_connectivity.rc \
+    ueventd.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 34
 
