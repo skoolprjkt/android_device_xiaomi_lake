5af83e3bb054a4ccab3c31b96d8b5fa5a4ff4db7
XeroMz69 <adamyd18@gmail.com>
2025-03-15 10:13:02 +0700

    lake: Initial tree
    Signed-off-by: XeroMz69 <adamyd18@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/lake
+
+# Inherit the proprietary files
+include vendor/xiaomi/lake/BoardConfigVendor.mk
---
+++ lineage_lake.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/lake/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_lake
+PRODUCT_DEVICE := lake
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 2409BRN2CA
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lake-user 14 UP1A.231005.007 V816.0.10.0.UGTMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/lake_global/lake:14/UP1A.231005.007/V816.0.10.0.UGTMIXM:user/release-keys
